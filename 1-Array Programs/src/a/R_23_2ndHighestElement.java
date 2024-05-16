7da5be4f8f304b2eaa9ccea506ca00973acb2c08
MS-Raunak <raunakkr.saha@gmail.com>
2024-05-16 22:04:46 +0530

    java array programs

---
+++ 1-Array Programs/src/a/R_23_2ndHighestElement.java
@@ -0,0 +1,11 @@
+package a;
+
+public class R_23_2ndHighestElement {
+
+	public static void main(String[] args) {
+		int[] a= {20,1,2,3,5,30,7,8,10,30,20};
+		System.out.println("2nd highest element is :"+A_Methods.secondHighest(a));
+
+	}
+
+}
---
+++ 1-Array Programs/src/a/T_36_RemoveDuplicated.java
@@ -0,0 +1,11 @@
+package a;
+
+public class T_36_RemoveDuplicated {
+
+	public static void main(String[] args) {
+		int[] a= {5,1,3,3,1,1,6,7};
+		A_Methods.removeDuplicates(a);
+
+	}
+
+}
